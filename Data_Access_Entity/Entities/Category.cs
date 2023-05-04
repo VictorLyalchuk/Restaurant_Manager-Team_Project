f22eaf564889ebd76eab34a353b100fd531004e3
VictorLyalchuk <121404620+VictorLyalchuk@users.noreply.github.com>
2023-05-04 23:10:38 +0300

    ListBoxWaiterConnect

--- Data_Access_Entity/Entities/Category.cs
+++ Data_Access_Entity/Entities/Category.cs
@@ -8,5 +8,9 @@ namespace Data_Access_Entity.Entities
         [Required]
         public string Name { get; set; }
         public ICollection<Product> Products { get; set; }
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
     }
 }
