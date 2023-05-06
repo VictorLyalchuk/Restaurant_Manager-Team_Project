8a1b555e0a32172dc705104be182aeb4d187c28a
temeirro <art.rozhyk@gmail.com>
2023-05-06 17:27:33 +0300

    added receipt and rating (with mvvm) pages

---
+++ Client_App/StaffRating.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Data_Access_Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client_App
+{
+    /// <summary>
+    /// Interaction logic for StaffRating.xaml
+    /// </summary>
+    public partial class StaffRating : Window
+    {
+        ViewModel model = new ViewModel();
+        RestaurantContext context = new RestaurantContext();
+        public StaffRating()
+        {
+            InitializeComponent();
+            GetWaiters();
+            DataContext = model;
+        }
+        #region adaptive borderless-window react
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+
+        }
+
+        private void pnlControlBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        #endregion
+        #region navigation bar buttons
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+                this.WindowState = WindowState.Maximized;
+            else this.WindowState = WindowState.Normal;
+        }
+
+        #endregion
+        void GetWaiters()
+        {
+            foreach (var item in context.Waiters.ToArray())
+            {
+                model.AddInWaiter(item);
+            }
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            Menu menu = new Menu();
+            this.Close();
+            menu.Show();
+        }
+    }
+}
