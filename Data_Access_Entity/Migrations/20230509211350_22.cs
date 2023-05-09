1af2ca016453397db5c61a74129669d7a9ce22a3
VictorLyalchuk <121404620+VictorLyalchuk@users.noreply.github.com>
2023-05-10 00:24:20 +0300

    fix

---
+++ Data_Access_Entity/Migrations/20230509211350_22.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data_Access_Entity.Migrations
+{
+    public partial class _22 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
